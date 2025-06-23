b5aecac2d03293a80bc841d59b74c9a4f8a2b5d7
jose <luciorans2003@gmail.com>
2025-06-23 16:44:14 -0600

    Primer commit

---
+++ js/animaciones.js
@@ -0,0 +1,32 @@
+function mostrarAlerta() {
+    Swal.fire({
+        title: "Ingredientes",
+        text: "Tortilla de harina de 30 cm, tocino, queso, arrachera, pimientos acompañado de cebolla morada, cebolla crocante",
+        imageUrl: "imagenes/papas.jpg",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+    });
+}
+
+function mostrarAlerta2() {
+    Swal.fire({
+        title: "Ingredientes",
+        text: "Chorizo argetino Barriga y Tuetano",
+        imageUrl: "imagenes/santito.jpeg",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+    });
+}
+
+function mostrarAlerta3() {
+    Swal.fire({
+        title: "Ingredientes",
+        text: "250 grs. arrachera, queso cheddar, queso gouda, mantequilla y aguacate.",
+        imageUrl: "imagenes/santo.jpg",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+    });
+}
